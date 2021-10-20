6c0253301dab3b13af2a77e0f35d46dfd7dfc846
Mellie Dee <89175650+MellieDee@users.noreply.github.com>
2021-10-19 21:27:15 -0700

    add prevent for form refreh

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
 //ID and elements in HTML
-var buttonEl = document.querySelector("#save-task");
+// var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-  //create taskHandler as shorthand for Btn function - do before so it is defiend by time btn sees it
-  var createTaskHandler =  function() {
+
+  //create taskHandler as shorthand for Btn function - do before so it is defined by time btn sees it
+  var createTaskHandler =  function(event) {
+  
+
+    event.preventDefault()
+
     var taskItemEl = document.createElement("li");
-    taskItemEl.textContent = "Start Challenge"
-    taskItemEl.className =  "task-item"
+    taskItemEl.textContent = "Start Challenge";
+    taskItemEl.className =  "task-item";
     tasksToDoEl.appendChild(taskItemEl);
   }
 
-//add new task by listening to button  and applying the desired action, nicknamed: creatTaskHandler
-buttonEl.addEventListener("click", createTaskHandler);
+//add new task by listening to form  and applying the desired action, nicknamed: creatTaskHandler
+formEl.addEventListener("submit", createTaskHandler);
