8e89eeca934a80dd67fb695eec9e4ad303860414
Mellie Dee <89175650+MellieDee@users.noreply.github.com>
2021-10-19 23:21:03 -0700

    add div, lis so lists type and task; deploy test good

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,15 +6,32 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
   //create taskHandler as shorthand for Btn function - do before so it is defined by time btn sees it
   var createTaskHandler =  function(event) {
-  
-
     event.preventDefault()
 
-    var taskItemEl = document.createElement("li");
-    taskItemEl.textContent = "Start Challenge";
-    taskItemEl.className =  "task-item";
-    tasksToDoEl.appendChild(taskItemEl);
+    //to find value for input field using console.dir to
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+
+    //create list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+
+    //create DIV to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+
+    //class name for DIV aka taskInfoEl
+    taskInfoEl.className = "task-info";
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span";
+
+    listItemEl.appendChild(taskInfoEl);
+
+ //add entire list item to list
+ tasksToDoEl.appendChild(listItemEl);
   }
 
 //add new task by listening to form  and applying the desired action, nicknamed: creatTaskHandler
 formEl.addEventListener("submit", createTaskHandler);
+
